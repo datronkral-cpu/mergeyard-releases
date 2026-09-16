54140a8c8e7f21884fd9d1401f1a39cfb2b4ab58
datronkral-cpu <datronkral@gmail.com>
2026-09-16 22:46:49 +0300

    web 0.3.56: 3x3 tahta, ayarlar duzeldi, sifirlama gorunur, kilavuz buyudu

--- oyna/index.service.worker.js
+++ oyna/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1789586930|5236192';
+const CACHE_VERSION = '1789588006|6264381';
 /** @type {string} */
 const CACHE_PREFIX = 'MergeYard-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
