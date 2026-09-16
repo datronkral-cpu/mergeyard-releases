d238c72a3a45e020882f6f87483ea60d584c107d
datronkral-cpu <datronkral@gmail.com>
2026-09-16 20:22:33 +0300

    web: markali acilis ekrani (Godot logosu yok), telemetri basari kontrolu duzeltildi, fenerci zipla+titret

--- oyna/index.service.worker.js
+++ oyna/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1789578681|5644571';
+const CACHE_VERSION = '1789579343|5315171';
 /** @type {string} */
 const CACHE_PREFIX = 'MergeYard-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
