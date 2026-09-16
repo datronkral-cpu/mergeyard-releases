b87827455df71f8a8d6ff0a7ecbbad9b547b0760
datronkral-cpu <datronkral@gmail.com>
2026-09-16 20:11:33 +0300

    web: telemetri teshis gostergesi + cross-origin isolation kapali

--- oyna/index.service.worker.js
+++ oyna/index.service.worker.js
@@ -4,14 +4,14 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1789578410|5350284';
+const CACHE_VERSION = '1789578681|5644571';
 /** @type {string} */
 const CACHE_PREFIX = 'MergeYard-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 /** @type {string} */
 const OFFLINE_URL = 'index.offline.html';
 /** @type {boolean} */
-const ENSURE_CROSSORIGIN_ISOLATION_HEADERS = true;
+const ENSURE_CROSSORIGIN_ISOLATION_HEADERS = false;
 // Files that will be cached on load.
 /** @type {string[]} */
 const CACHED_FILES = ["index.html","index.js","index.offline.html","index.icon.png","index.apple-touch-icon.png","index.audio.worklet.js","index.audio.position.worklet.js"];
